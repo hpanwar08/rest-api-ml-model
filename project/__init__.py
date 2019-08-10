69f4958a4f3c4c0e3ab070d579d13a4f378ddf7e
himanshu <heem.86@gmail.com>
2019-08-10 08:40:57 +0530

    logging

--- project/__init__.py
+++ project/__init__.py
@@ -5,8 +5,8 @@ from project.api.endpoints.restplus import api
 
 def create_app(script_info=None):
     app = Flask(__name__)
-    app_settings = os.getenv("APP_SETTINGS")
-    app.config.from_object(app_settings)
+    # app_settings = os.getenv("APP_SETTINGS")
+    # app.config.from_object(app_settings)
 
     from project.api.endpoints.sentiment import ns as sentiment_blueprint
 
